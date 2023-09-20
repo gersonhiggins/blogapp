7bae06c343fab37165ad6dac8fccf061f1eb9189
gersongahg <gahg.10gerson@gmail.com>
2023-09-20 13:50:19 -0400

    Mehtods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: 'author_id'
+    has_many :posts, foreign_key: 'user_id'
     has_many :comments
     has_many :likes
+
+    def recent_posts(count)
+      posts.order(created_at: :desc).limit(count)
+    end
   end
