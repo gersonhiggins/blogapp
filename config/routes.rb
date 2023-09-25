dad0b61c2ab687cabc18a472571af56ec0e33934
gersongahg <gahg.10gerson@gmail.com>
2023-09-25 19:51:15 -0400

    requested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-
-  # Route for listing all posts for a user
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-
-  # Route for showing a specific post for a user
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  # Users
+  resources :users, only: [:index, :show] do
+    # Posts for a specific user
+    resources :posts, only: [:index, :show]
+  end
 end
